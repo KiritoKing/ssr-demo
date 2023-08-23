f3b5585ca06427bb5f3b93797b3f7c1e97c02512
chenliangzihao <chenliangzihao@bytedance.com>
2023-08-23 17:23:17 +0800

    init

---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { renderToString } from "react-dom/server";
+import React from "react";
+import App from "../components/App";
+
+const app = express(); // {1}
+app.use(express.static("public")); // {2}
+const content = renderToString(<App />); //{3}
+
+app.get("/", function (req, res) {
+  // {4}
+  res.send(`
+    <!doctype html>
+    <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport"
+                content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>React SSR</title>
+      </head>
+      <body>
+        <div id="root">${content}</div>
+        <script src="/index.js"></script>  
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000);
